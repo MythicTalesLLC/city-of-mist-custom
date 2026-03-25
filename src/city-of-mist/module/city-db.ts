53a32722cad67cc58d19c9f8007138e5df8138b6
Dan Bond <mythic@Dans-MacBook-Air.local>
2026-03-25 07:53:33 -0400

    chore: finalize Improvement #1 source state

--- src/city-of-mist/module/city-db.ts
+++ src/city-of-mist/module/city-db.ts
@@ -9,12 +9,12 @@ import { Move } from "./city-item.js";
 import { CityItem } from "./city-item.js";
 import { CityActor } from "./city-actor.js";
 import { Themebook } from "./city-item.js";
-import {DBAccessor} from "./tools/db-accessor.js";
+import { DBAccessor } from "./tools/db-accessor.js";
 
 declare global {
 	interface HOOKS {
-		"cityDBLoaded": ()=> void;
-		"themebooksLoaded": ()=> void;
+		"cityDBLoaded": () => void;
+		"themebooksLoaded": () => void;
 		"movesLoaded": () => void;
 	}
 }
@@ -24,8 +24,8 @@ export class CityDB extends DBAccessor {
 	static movesList: Move[] = [];
 	static _dangerTemplates: Danger[] = [];
 	static _loaded = false;
-	static _allThemebooks : Themebook[] =[];
-	static _systemThemebooks : Themebook[] =[];
+	static _allThemebooks: Themebook[] = [];
+	static _systemThemebooks: Themebook[] = [];
 	static _systemTutorial: U<JournalEntry>;
 
 	static override async loadPacks() {
@@ -39,7 +39,7 @@ export class CityDB extends DBAccessor {
 			this._loaded = true;
 		} catch (e) {
 			console.error(`Error Loading Packs - potentially try a browser reload \n ${e}`);
-			setTimeout( () => this.loadPacks(), 5000);
+			setTimeout(() => this.loadPacks(), 5000);
 			throw e;
 		}
 	}
@@ -47,29 +47,29 @@ export class CityDB extends DBAccessor {
 	static async loadTutorial() {
 		const arr = await this.getCompendiumDataByType("JournalEntry") as JournalEntry[];
 		//@ts-expect-error bad foundry types
-		this._systemTutorial = arr.find( x=> x.name == "System Tutorial");
+		this._systemTutorial = arr.find(x => x.name == "System Tutorial");
 		if (!this._systemTutorial) {
 			console.warn("Couldn't locate system tutorial document");
 		}
 	}
 
 	static async waitUntilLoaded(): Promise<void> {
-		if (this._loaded) {return;}
-		return new Promise( (conf, rej) => {
+		if (this._loaded) { return; }
+		return new Promise((conf, rej) => {
 			let count = 0;
-			const x = setInterval( () => {
+			const x = setInterval(() => {
 				if (this._loaded) {
 					clearInterval(x);
 					conf();
 				}
 				if (count++ > 20) {
-					rej( new DataBaseTimeOutError("Database load Timeout"));
+					rej(new DataBaseTimeOutError("Database load Timeout"));
 				}
 			}, 500);
 		});
 	}
 
-	static isLoaded() : boolean {
+	static isLoaded(): boolean {
 		return this._loaded;
 	}
 
@@ -90,7 +90,7 @@ export class CityDB extends DBAccessor {
 			throw new Error("ERROR: No Valid themebooks found");
 		}
 		const system = CitySettings.get("baseSystem");
-		return CityDB._themebooks.filter( x=> x.isSystemCompatible(system));
+		return CityDB._themebooks.filter(x => x.isSystemCompatible(system));
 	}
 
 	static loadThemebooks() {
@@ -103,8 +103,8 @@ export class CityDB extends DBAccessor {
 		return true;
 	}
 
-	static filterOverridedContent<T extends Move | Themebook>(list : T[]): T[] {
-		return list.filter( x=> !x.system.free_content || !list.some(y=>
+	static filterOverridedContent<T extends Move | Themebook>(list: T[]): T[] {
+		return list.filter(x => !x.system.free_content || !list.some(y =>
 			x != y
 			&& y.name == x.name
 			&& !y.system.free_content
@@ -112,24 +112,21 @@ export class CityDB extends DBAccessor {
 	}
 
 	static getLoadoutThemebook(): Themebook | undefined {
-		const themebooks = this.themebooks.filter( x=>
+		const themebooks = this.themebooks.filter(x =>
 			SystemModule.isLoadoutThemeType(x.system.subtype)
 			&& x.isSystemCompatible(CitySettings.getBaseSystem())
 		);
-		if (themebooks.some( x=> !x.system.free_content))
-			{return themebooks.filter(x=> !x.system.free_content)[0];}
-		if (themebooks.length)
-			{return themebooks[0];}
-		else
-			{return undefined;}
+		if (themebooks.some(x => !x.system.free_content)) { return themebooks.filter(x => !x.system.free_content)[0]; }
+		if (themebooks.length) { return themebooks[0]; }
+		else { return undefined; }
 	}
 
-	static loadMovesOfType(movetype : Move["system"]["category"]) {
+	static loadMovesOfType(movetype: Move["system"]["category"]) {
 		let movesList = this.filterItemsByType("move") as Move[];
 		movesList = this.filterOverridedContent(movesList);
-		movesList = movesList.filter( x=> x.system.category == movetype);
+		movesList = movesList.filter(x => x.system.category == movetype);
 		const include = CitySettings.get("movesInclude") ?? "city-of-mist";
-		return movesList.filter( x=> x.isSystemCompatible(include));
+		return movesList.filter(x => x.isSystemCompatible(include));
 	}
 
 	static loadMoves() {
@@ -139,7 +136,7 @@ export class CityDB extends DBAccessor {
 		this.movesList = core
 			.concat(advanced)
 			.concat(SHB)
-			.sort( (a,b) => a.name.localeCompare(b.name));
+			.sort((a, b) => a.name.localeCompare(b.name));
 		Hooks.callAll("movesLoaded");
 		return true;
 	}
@@ -150,66 +147,61 @@ export class CityDB extends DBAccessor {
 
 	static refreshDangerTemplates() {
 		this._dangerTemplates = (this.filterActorsByType("threat") as CityActor[])
-			.filter( x=> x.system.type == "threat" && x.system.is_template) as Danger[];
+			.filter(x => x.system.type == "threat" && x.system.is_template) as Danger[];
 	}
 
-	static getDangerTemplate(id : string): Danger | undefined {
-		return this._dangerTemplates.find( x=> x.id  == id);
+	static getDangerTemplate(id: string): Danger | undefined {
+		return this._dangerTemplates.find(x => x.id == id);
 	}
 
 	static getTagOwnerById(tagOwnerId: string) {
-		const val = game.actors.find(x=> x.id == tagOwnerId)
-			|| game.scenes.find( x=> x.id == tagOwnerId);
-		if (val)
-			{return val;}
-		else
-			{throw new Error(`Couldn't find tag owner for Id ${tagOwnerId}`);}
+		const val = game.actors.find(x => x.id == tagOwnerId)
+			|| game.scenes.find(x => x.id == tagOwnerId);
+		if (val) { return val; }
+		else { throw new Error(`Couldn't find tag owner for Id ${tagOwnerId}`); }
 	}
 
-	static getBuildUpImprovements() : Improvement[] {
+	static getBuildUpImprovements(): Improvement[] {
 		const list = this.filterItemsByType("improvement") as Improvement[];
 		const system = CitySettings.getBaseSystem();
-		return list.filter( item => {
+		return list.filter(item => {
 			if (!item.isSystemCompatible(system)) {
 				return false;
 			}
-			const nameFilter = list.filter( x=> x.name == item.name);
-			if (nameFilter.length == 1)
-				{return true;}
-			else
-				{return !item.system.free_content;}
+			const nameFilter = list.filter(x => x.name == item.name);
+			if (nameFilter.length == 1) { return true; }
+			else { return !item.system.free_content; }
 		});
 	}
 
-	static getMoveById(moveId: string) : Move | undefined {
-		return CityDB.movesList.find(x=> x.id == moveId);
+	static getMoveById(moveId: string): Move | undefined {
+		return CityDB.movesList.find(x => x.id == moveId);
 	}
 
-	static getThemebook(tname: string, id?:string) : Themebook {
+	static getThemebook(tname: string, id?: string): Themebook {
 		let book: Themebook | undefined;
 		book = this.searchForContent(this._themebooks, id, tname);
-		if (book) {return book;}
+		if (book) { return book; }
 		book = this.searchForContent(this._systemThemebooks, id, tname);
 		if (book) {
 			const updated = this.searchForContent(this._themebooks, id, book.name);
 			return updated ?? book;
 		}
 		book = this.searchForContent(this._allThemebooks, id, tname);
-		if (book) {return book;}
+		if (book) { return book; }
 		if (!book && id) {
 			//last resort search using old id system
 			// console.log("Using Old Style Search");
 			try {
 				const idconv = this.oldTBIdToName(id);
 				if (idconv) {
-					book= this.getThemebook (idconv);
-					if (book) {return book;}
+					book = this.getThemebook(idconv);
+					if (book) { return book; }
 				}
 				throw new Error(`Can't find themebook ${tname}: ${id}`);
 			} catch (e) {
 				// ui.notifications.warn(`Couldn't get themebook for ${tname}, try refreshing your browser window (F5)`);
-				if (e instanceof Error)
-					{console.log(e.stack);}
+				if (e instanceof Error) { console.log(e.stack); }
 				throw e;
 			}
 		}
@@ -217,38 +209,38 @@ export class CityDB extends DBAccessor {
 		throw new Error(`Couldn't get themebook for ${tname} :  ${id}`);
 	}
 
-	static searchForContent<T extends CityItem>(arr: T[], id?: string, name ?: string): T | undefined {
-		const answer = arr.find(x=> x.id == id);
-		if (answer) {return answer;}
-		else {return arr.find( x=> x.name == name);}
+	static searchForContent<T extends CityItem>(arr: T[], id?: string, name?: string): T | undefined {
+		const answer = arr.find(x => x.id == id);
+		if (answer) { return answer; }
+		else { return arr.find(x => x.name == name); }
 	}
 
-	static oldTBIdToName(id: string) : string | undefined {
+	static oldTBIdToName(id: string): string | undefined {
 		// converts Beta version ids into names
 		// ugly code for backwards compatiblity
 		switch (id) {
-			case "wpIdnVs3F3Z2pSgX" : return "Adaptation";
-			case "0MISdMEFLyxmDpl4" : return "Bastion";
-			case "AKafVzAawzfJyfPE" : return "Conjuration";
-			case "xZcNJ8mKpLr4vB1w" : return "Conjunction"; // S&S Logos themebook
-			case "rSJ8sbrz2nQXKNTx" : return "Crew Theme";
-			case "G6U7gXAECea110Be" : return "Defining Event";
-			case "gP7G0S8vIhW95w0k" : return "Defining Relationship";
-			case "Kgle3kIF3JMftKWI" : return "Destiny";
-			case "NTarcKas0Ud1YKsM" : return "Divination";
-			case "XPcAouNdmrZEzo4d" : return "Enclave";
-			case "FZiP2EhayfY7Ii66" : return "Expression";
-			case "f38Z3OI3cCPoVUyD" : return "Familiar";
-			case "dScP2BYdyr9X9MAG" : return "Mission";
-			case "BXpouQf9TVvxoFFV" : return "Mobility";
-			case "pPZ52M16SoYfqbFY" : return "Personality";
-			case "jaINI4IYpHFZQPnD" : return "Possessions";
-			case "GFkmD7kCYdWquuaW" : return "Relic";
-			case "O2KUvX351pRE3tZd" : return "Routine";
-			case "1D6OuTZCZoOygiRp" : return "Struggle";
-			case "kj7MU8YgUzkbC7BF" : return "Subversion";
-			case "DtP21Q36GuCLDMeL" : return "Training";
-			case "zoOtXbPteK6gkObm" : return "Turf";
+			case "wpIdnVs3F3Z2pSgX": return "Adaptation";
+			case "0MISdMEFLyxmDpl4": return "Bastion";
+			case "AKafVzAawzfJyfPE": return "Conjuration";
+			case "xZcNJ8mKpLr4vB1w": return "Conjunction"; // S&S Logos themebook
+			case "rSJ8sbrz2nQXKNTx": return "Crew Theme";
+			case "G6U7gXAECea110Be": return "Defining Event";
+			case "gP7G0S8vIhW95w0k": return "Defining Relationship";
+			case "Kgle3kIF3JMftKWI": return "Destiny";
+			case "NTarcKas0Ud1YKsM": return "Divination";
+			case "XPcAouNdmrZEzo4d": return "Enclave";
+			case "FZiP2EhayfY7Ii66": return "Expression";
+			case "f38Z3OI3cCPoVUyD": return "Familiar";
+			case "dScP2BYdyr9X9MAG": return "Mission";
+			case "BXpouQf9TVvxoFFV": return "Mobility";
+			case "pPZ52M16SoYfqbFY": return "Personality";
+			case "jaINI4IYpHFZQPnD": return "Possessions";
+			case "GFkmD7kCYdWquuaW": return "Relic";
+			case "O2KUvX351pRE3tZd": return "Routine";
+			case "1D6OuTZCZoOygiRp": return "Struggle";
+			case "kj7MU8YgUzkbC7BF": return "Subversion";
+			case "DtP21Q36GuCLDMeL": return "Training";
+			case "zoOtXbPteK6gkObm": return "Turf";
 		}
 	}
 
@@ -256,108 +248,102 @@ export class CityDB extends DBAccessor {
 	// ******************   Hooks  ******************* *
 	// **************************************************
 
-	static onItemUpdate(item:CityItem, _updatedItem:unknown, _data:unknown, _diff:unknown) {
+	static onItemUpdate(item: CityItem, _updatedItem: unknown, _data: unknown, _diff: unknown) {
 		const actor = item.parent as CityActor;
-		if (actor)
-			{for (const dep of actor.getDependencies()) {
+		if (actor) {
+			for (const dep of actor.getDependencies()) {
 				const sheet = dep.sheet;
 				if (sheet._state > 0) {
 					CityHelpers.refreshSheet(dep);
 				}
-			}}
+			}
+		}
 		return true;
 	}
 
-	static onActorUpdate(actor:CityActor, _updatedItem:unknown, _data:unknown, _diff:unknown) {
+	static onActorUpdate(actor: CityActor, _updatedItem: unknown, _data: unknown, _diff: unknown) {
 		for (const dep of actor.getDependencies()) {
 			const sheet = dep.sheet;
-			if (sheet._state  > 0) {
+			if (sheet._state > 0) {
 				console.log(`Refreshing sheet of ${dep.name}`);
 				CityHelpers.refreshSheet(dep);
 			}
 		}
-		if (actor.isDanger())
-			{this.refreshDangerTemplates();}
+		if (actor.isDanger()) { this.refreshDangerTemplates(); }
 		return true;
 	}
 
 	static async onTokenDelete(token: TokenDocument<Danger>) {
 		await this.onTokenUpdate(token, {}, {});
 		if (token.actor && token.actorLink) {
-			if (token.actor.hasEntranceMoves() && !token.hidden)
-				{await token.actor.undoEntranceMoves(token);}
+			if (token.actor.hasEntranceMoves() && !token.hidden) { await token.actor.undoEntranceMoves(token); }
 		}
 		return true;
 	}
 
-	static async onTokenUpdate(token : TokenDocument<Danger>, changes?: Record<string, unknown>, _otherStuff?: unknown) {
-		if (!token.actor) {return;}
-		if (changes?.hidden === false && token.actor.hasEntranceMoves())
-			{await token.actor.executeEntranceMoves(token);}
-		if (changes?.hidden === true && token.actor.hasEntranceMoves())
-			{await token.actor.undoEntranceMoves(token);}
-		if (game.scenes.active != token.parent)
-			{return;}
+	static async onTokenUpdate(token: TokenDocument<Danger>, changes?: Record<string, unknown>, _otherStuff?: unknown) {
+		if (!token.actor) { return; }
+		if (changes?.hidden === false && token.actor.hasEntranceMoves()) { await token.actor.executeEntranceMoves(token); }
+		if (changes?.hidden === true && token.actor.hasEntranceMoves()) { await token.actor.undoEntranceMoves(token); }
+		if (game.scenes.active != token.parent) { return; }
 		await CityHelpers.refreshTokenActorsInScene(token.parent);
 		return true;
 	}
 
 	static async onTokenCreate(token: TokenDocument<CityActor>) {
-		if (!token.actor) {return;}
+		if (!token.actor) { return; }
 		const type = token.actor.system.type;
-		if (type == "character" || type == "crew" )
-			{await CityHelpers.ensureTokenLinked(token.parent, token);}
+		if (type == "character" || type == "crew") { await CityHelpers.ensureTokenLinked(token.parent, token); }
 		if (type == "threat") {
 			const danger = token as TokenDocument<Danger>;
 			await this.onTokenUpdate(danger);
-			if (danger.actor!.hasEntranceMoves()  && !danger.hidden) {
+			if (danger.actor!.hasEntranceMoves() && !danger.hidden) {
 				await token.actor.executeEntranceMoves(danger);
 			}
 		}
 		return true;
 	}
 
-	static async onSceneUpdate(scene: Scene, changes: {active?:boolean}) {
-		if (!changes.active) {return;}
+	static async onSceneUpdate(scene: Scene, changes: { active?: boolean }) {
+		if (!changes.active) { return; }
 		await CityHelpers.refreshTokenActorsInScene(scene);
 		return true;
 	}
 
-	static essences() : Essence [] {
-		const essences = this.allItems().filter( item =>
+	static essences(): Essence[] {
+		const essences = this.allItems().filter(item =>
 			item.system.type == "essence" &&
 			item.isCompatible()
 		) as Essence[];
 		return essences.filter(this.filterBestVersion);
 	}
 
-	static getEssence(id: string) : Essence | undefined {
-		return this.essences().find( item => item.id == id);
+	static getEssence(id: string): Essence | undefined {
+		return this.essences().find(item => item.id == id);
 	}
 
-	static getEssenceBySystemName(name: keyof EssenceNames) : Essence | undefined {
-		return this.essences().find( item =>
+	static getEssenceBySystemName(name: keyof EssenceNames): Essence | undefined {
+		return this.essences().find(item =>
 			item.system.systemName == name);
 	}
 
-	static override allItems() : CityItem[] {
+	static override allItems(): CityItem[] {
 		return super.allItems() as CityItem[];
 	}
 
-	static filterBestVersion< T extends Overrideable>(this: void, item: T, _index: number, arr:  T[]) {
-		const others = arr.filter(x=> x!= item && CityDB.overrideableEqualityTest(item, x));
-		if (others.length == 0)
-		{ //if there is only one kind, use that
+	static filterBestVersion<T extends Overrideable>(this: void, item: T, _index: number, arr: T[]) {
+		const others = arr.filter(x => x != item && CityDB.overrideableEqualityTest(item, x));
+		if (others.length == 0) { //if there is only one kind, use that
 			return true;
 		}
-		if (item.system.free_content && others.some( other=> !other.system.free_content)) {
+		if (item.system.free_content && others.some(other => !other.system.free_content)) {
 			return false;
 		}
 		return true;
 	}
 
-	static overrideableEqualityTest < T extends Overrideable> (item1: T, item2: T) {
-		if (item1.system.type != item2.system.type) {return false;}
+	static overrideableEqualityTest<T extends Overrideable>(item1: T, item2: T) {
+		if (item1.system.type != item2.system.type) { return false; }
 		return item1.systemName == item2.systemName;
 	}
 
@@ -382,4 +368,4 @@ window.CityDB = CityDB;
 declare global {
 }
 
-class DataBaseTimeOutError extends Error {}
+class DataBaseTimeOutError extends Error { }
